e27b2454bd146a7e4402f084534e2a6161719f47
DESKTOP-00HANON\sunds <sundstrom.markus@gmail.com>
2022-09-06 13:04:49 +0200

    Exempel 1A v1

---
+++ web/1a/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h1>Hej!</h1>
+<form action="print.php" method="post">
+    Namn: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    Telefon: <input type="text" name=phone><br>
+    <input type="submit" value="Skicka">
+</form>
+</body>
+</html>
---
+++ web/1a/print.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+if (isset($_POST)) {
+	echo "Namn: " . $_POST['name'] . "<br>";
+	echo "E-mail: " . $_POST['email'] . "<br>";
+	echo "Telefon: " . $_POST['phone'] . "<br>";
+}
+?>
+</body>
+</html>
+
+
+
+
