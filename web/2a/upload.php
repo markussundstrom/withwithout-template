f5a30956c95f4373d41e69dc15d7c6fbc7533152
Markus Sundström <sundstrom.markus@gmail.com>
2022-09-07 09:24:12 +0200

    Exempel 2A v2

--- web/2a/upload.php
+++ web/2a/upload.php
@@ -6,13 +6,13 @@ $target_file = $target_dir . basename($_FILES['uploaded_file']['name']);
 
 if(isset($_POST['submit'])) {
     if($_FILES['uploaded_file']['size'] > 700000) {
-        echo "Error, file is too large";
+        echo "The file is too large!";
     } else {
         $type_of_file = exif_imagetype($_FILES['uploaded_file']['tmp_name']);
         if (($type_of_file == IMAGETYPE_JPEG) || ($type_of_file == IMAGETYPE_PNG)) {
-            echo "Upload OK";
+            echo "Thank you! The upload is OK";
         } else {
-            echo "Upload not OK";
+            echo "The file is not a JPEG or PNG!";
         }
     }
 }
